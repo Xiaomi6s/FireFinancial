b834b316349d255ba9c838871fe3f794dc75a639
renxiaojian <renxiaojian@yonglibao.com>
2017-08-03 11:43:45 +0800

    Initial Commit

---
+++ FireFinancial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FireFinancial
+//
+//  Created by 任晓健 on 2017/8/3.
+//  Copyright © 2017年 rxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FireFinancial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FireFinancial
+//
+//  Created by 任晓健 on 2017/8/3.
+//  Copyright © 2017年 rxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
