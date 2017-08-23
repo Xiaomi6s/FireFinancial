7ae91aeb16e34236d94ffce362d65945996a3b3e
刘红梅 <1610764307@qq.com>
2017-08-23 14:50:44 +0800

    basic  info

---
+++ FireFinancial/Apis/FFBasicInfo.h
@@ -0,0 +1,13 @@
+//
+//  FFBasicInfo.h
+//  FireFinancial
+//
+//  Created by 刘红梅 on 2017/8/23.
+//  Copyright © 2017年 rxj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFBasicInfo : NSObject
+
+@end
---
+++ FireFinancial/Apis/FFUserInfo.h
@@ -0,0 +1,13 @@
+//
+//  FFUserInfo.h
+//  FireFinancial
+//
+//  Created by 刘红梅 on 2017/8/23.
+//  Copyright © 2017年 rxj. All rights reserved.
+//
+
+#import "FFBasicInfo.h"
+
+@interface FFUserInfo : FFBasicInfo
+
+@end
