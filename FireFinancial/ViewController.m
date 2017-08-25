db0cdb280c0755d64962256bcedd2de01c07d14b
renxiaojian <renxiaojian@yonglibao.com>
2017-08-25 09:59:27 +0800

    ...

--- FireFinancial/ViewController.m
+++ FireFinancial/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 #import "MBProgressHUD+FFXJ.h"
 #import "FFBasicApi.h"
+#import "RSAEncryptor.h"
+#import "NSString+AES.h"
 @interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
 
 @property(nonatomic, strong) UITableView *tableView;
@@ -29,6 +31,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *rsaStr = [RSAEncryptor encryptString:@"深圳市信融财富投资管理有限公司创办于2012年3月，注册资本6904.55万元人民币，实收资本5704.55万元人民币，总部位于深圳，拥有完整的产品、研发、风控、财务以及客服等职能体系，是一家具备优秀的自主研发能力的金融创新型企业，在2014年当选广东互联网金融协会首届副会长单位。我们旨在通过为投资人提供低风险、收益率适中的投资理财服务，帮助其实现资产的稳健增值；同时，通过为融资方高效解决资金需求，帮助其改善生产和生活" publicKey:@"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDPrKMIntSVHnZJBudwhpNj/X6jo9VyeK4GK3db61VN1hsAbgiJvCEwLSSLXeJldvM5Ru6c1tVtWkkGATU2QXVpWHtvajqDFvZTPKI+Hb9P6RMZk2QWic49D1+heCeAuT96ZCEkjKNNAgd4ysVh6Aav9wpi5WSi45a99ljuKQuZpQIDAQAB"];
+    NSString *aesStr = @"NG9OZHZoVWViY29qK2pGSWk5a0U1THpDNWtsWVJBWWl1TW5RMm15UjBJaz0=";
+    NSData *aesData = [aesStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *aesData2 = [[NSData alloc] initWithBase64EncodedData:aesData options:0];
+    NSString *str11 = [[NSString alloc] initWithData:aesData2 encoding:NSUTF8StringEncoding];
+    
+    NSString *str3 = [str11 AES_decrypt];
+    
     self.title = @"apis";
     [self loadData];
     [self.view addSubview:self.tableView];
