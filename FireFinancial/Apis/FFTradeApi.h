5f066bdf3cb4441050e064c029a4576227c52665
刘红梅 <1610764307@qq.com>
2017-08-29 15:49:41 +0800

    no message

--- FireFinancial/Apis/FFTradeApi.h
+++ FireFinancial/Apis/FFTradeApi.h
@@ -10,4 +10,13 @@
 
 @interface FFTradeApi : FFBasicApi
 
+///**
+// 获取今日撮合年化利率
+//
+// @param type 类型 1:综合 2:零活宝 3:定期宝
+// @param finished 回调
+// */
+//- (void)getTradeTodayRateInfoWithType:(NSString *)type
+//                          returnBlock:(FinishedBlock)finished;
+
 @end
--- FireFinancial/Apis/FFTradeApi.m
+++ FireFinancial/Apis/FFTradeApi.m
@@ -8,6 +8,25 @@
 
 #import "FFTradeApi.h"
 
+static NSString * const projectTodayRateApi = @"/project/todayRate";
+
 @implementation FFTradeApi
+
+/**
+ 获取今日撮合年化利率
+ 
+ @param type 类型 1:综合 2:零活宝 3:定期宝
+ @param finished 回调
+ */
+//- (void)getTradeTodayRateInfoWithType:(NSString *)type
+//                          returnBlock:(FinishedBlock)finished {
+//    NSDictionary *param = @{@"type":type};
+//
+////    [self asyncPostRequestWithUrl:projectTodayRateApi parameters:param infoclass:[FFRegisterShowInfo class] finished:^(FFRequestStatus status, id response) {
+////        finished(status, response);
+////    }];
+//    
+//}
+
 
 @end
